c80963c26ee3177c0ddd646512278b70faa9e5f5
Apheru <pythonic.apheru@gmail.com>
2024-10-24 09:54:04 -0300

    first commit

---
+++ mundo_01_fundamentos/exercicios/ex001.py
@@ -0,0 +1,8 @@
+# DESAFIO 001
+
+# Crie um programa que escreva "Olá, Mundo!" na tela.
+
+print("Olá, Mundo!")
+
+msg = "Olá, Mundo!"
+print(msg)
---
+++ mundo_01_fundamentos/exercicios/ex002.py
@@ -0,0 +1,6 @@
+# DESAFIO 02
+
+# Crie um programa que leia o nome do usuário e após apresente uma mensagem de boas-vindas.
+
+name = input("Digite seu nome: ")
+print(f"É um prazer te conhecer, {name}!")
